eca52e7409ae81dbe78889211b980709cb4c37c6
GabrielBahia <gabriel.bahia@estudante.ufjf.br>
2022-09-20 10:57:55 -0300

    Implementando Controller de Membros

---
+++ src/controllers/membrosController.js
@@ -0,0 +1,65 @@
+import membros from "../models/Membro.js";
+
+class MembroController {
+
+  static index = (req, res) => {
+    membros.find()
+      .exec((err, membros) => {
+        res.status(200).json(membros)
+  })
+  }
+
+  static show = (req, res) => {
+    const id = req.params.id;
+
+    membros.findById(id)
+      .exec((err, membros) => {
+      if(err) {
+        res.status(400).send({message: `${err.message} - Id do membro não localizado.`})
+      } else {
+        res.status(200).send(membros);
+      }
+    })
+  }
+
+  static store = (req, res) => {
+    let membro = new membros(req.body);
+
+    membro.save((err) => {
+
+      if(err) {
+        res.status(500).send({message: `${err.message} - falha ao cadastrar membro.`})
+      } else {
+        res.status(201).send(membro.toJSON())
+      }
+    })
+  }
+
+  static update = (req, res) => {
+    const id = req.params.id;
+
+    membros.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+      if(!err) {
+        res.status(200).send({message: 'Membro atualizado com sucesso'})
+      } else {
+        res.status(500).send({message: err.message})
+      }
+    })
+  }
+
+  static delete = (req, res) => {
+    const id = req.params.id;
+
+    membros.findByIdAndDelete(id, (err) => {
+      if(!err){
+        res.status(200).send({message: 'Membro removido com sucesso'})
+      } else {
+        res.status(500).send({message: err.message})
+      }
+    })
+  }
+
+
+}
+
+export default MembroController
