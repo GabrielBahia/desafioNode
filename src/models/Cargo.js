3d3dd4ff0994426803f712ac4c71f531c808b3c9
GabrielBahia <gabriel.bahia@estudante.ufjf.br>
2022-09-20 10:47:51 -0300

    Implementando Model de Cargo

---
+++ src/models/Cargo.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const cargoSchema = new mongoose.Schema(
+  {
+    id: {type: String},
+    nome: {type: String, required: true},
+  }
+);
+
+const cargos = mongoose.model('cargos', cargoSchema);
+
+export default cargos;
