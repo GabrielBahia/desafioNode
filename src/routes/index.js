044c611af3c7c6d648a63ce9a437a6c666e3a7b6
GabrielBahia <gabriel.bahia@estudante.ufjf.br>
2022-09-20 11:04:57 -0300

    Criando Route index

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import membros from "./membrosRoutes.js"
+import departamentos from "./departamentosRoutes.js"
+import cargos from "./cargosRoutes.js"
+
+const routes = (app) => {
+  app.route('/').get((req, res) => {
+    res.status(200).send({titulo: "CRUD de membros"})
+  })
+
+  app.use(
+    express.json(),
+    membros,
+    departamentos,
+    cargos
+  )
+}
+
+export default routes
