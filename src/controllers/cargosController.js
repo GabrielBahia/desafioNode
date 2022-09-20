993320f89eb9ca3cf266be0624f1a68f8a9a10b3
GabrielBahia <gabriel.bahia@estudante.ufjf.br>
2022-09-20 11:20:37 -0300

    Implementando Controller de Cargos

---
+++ src/controllers/cargosController.js
@@ -0,0 +1,65 @@
+import cargos from "../models/Cargo";
+
+class CargoController {
+
+  static index = (req, res) => {
+    cargos.find()
+      .exec((err, cargos) => {
+        res.status(200).json(cargos)
+  })
+  }
+
+  static show = (req, res) => {
+    const id = req.params.id;
+
+    cargos.findById(id)
+      .exec((err, cargos) => {
+      if(err) {
+        res.status(400).send({message: `${err.message} - Id do cargo não localizado.`})
+      } else {
+        res.status(200).send(cargos);
+      }
+    })
+  }
+
+  static store = (req, res) => {
+    let cargo = new cargos(req.body);
+
+    cargo.save((err) => {
+
+      if(err) {
+        res.status(500).send({message: `${err.message} - falha ao cadastrar cargo.`})
+      } else {
+        res.status(201).send(cargo.toJSON())
+      }
+    })
+  }
+
+  static update = (req, res) => {
+    const id = req.params.id;
+
+    cargos.findByIdAndUpdate(id, {$set: req.body}, (err) => {
+      if(!err) {
+        res.status(200).send({message: 'Cargo atualizado com sucesso'})
+      } else {
+        res.status(500).send({message: err.message})
+      }
+    })
+  }
+
+  static delete = (req, res) => {
+    const id = req.params.id;
+
+    cargos.findByIdAndDelete(id, (err) => {
+      if(!err){
+        res.status(200).send({message: 'Cargo removido com sucesso'})
+      } else {
+        res.status(500).send({message: err.message})
+      }
+    })
+  }
+
+
+}
+
+export default CargoController
